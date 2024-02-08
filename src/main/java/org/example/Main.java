9ce53fa43da2682a9cec628ccff29508b8d6822f
Uday <budayteja009@gmail.com>
2024-02-08 13:43:42 +0530

    Remove unused code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,137 +1,7 @@
-package org.example;/*
-String[] classNames = {
-        // List of class names goes here
-        "Nonprofit", "Address", "FiscalSponsor", "NonprofitAdditionalInfo", "NonprofitIRSData", "NonprofitMetadata",
-        "OfacFlags", "OfacSdn", "PlaidData", "DeleteFileInfo", "Foundation", "FoundationUser", "NonprofitUser",
-        "PresignedFile", "UploadedFile", "UserFoundationRole", "UserNonprofitRole", "AppUser", "NylasSyncRequest",
-        "AdditionalContact", "FoundationNonprofitInteraction", "IRS990Details", "InteractionAdditionalInfo",
-        "QBVendorDetails", "AnvilPDFAdditionalInfo", "AnvilPDFField", "Budget", "BudgetAllocation",
-        "CRMConfiguration", "CRMConfigurationField", "CRMExternalConfigProperty", "ConfigurationSettings",
-        "CustomEmailTemplate", "CustomGrantType", "DefaultDateSetting", "DisplayCondition", "DocumentItem",
-        "ExternalReviewerConfiguration", "ExternalReviewerTableConfiguration", "FeatureSet", "FormItem",
-        "FormItemOptions", "FormItemValue", "FormOptionEntity", "FoundationCRMField", "FoundationMailSettings",
-        "FoundationMetadata", "FoundationMetadataTag", "FoundationQuickbooksSetting", "FoundationReviewerUser",
-        "FoundationSalesforceConfiguration", "GrantStage", "GrantStageWorkflow", "GrantTypeConfiguration",
-        "GranteeProfileUpdate", "GranteeProfileUpdateConfiguration", "MailChimpFoundationConfiguration",
-        "MetabaseConfiguration", "MetabaseDashboardUrl", "Pipeline", "QBPaymentDestination", "QBPaymentSource",
-        "QBPaymentSyncSetting", "SalesforceEntityToUpdate", "SalesforceSyncRequest", "SalesforceTokenResponse",
-        "ScoringCriteria", "StageTaskTemplate", "TableDisplayCell", "TableDisplayContent", "TableDisplayRow",
-        "TaskTemplateAdditionalInfo", "TemelioSalesforceFieldMapping", "NonprofitIRSUpdateRequest",
-        "FormComponentSubmission", "FormItemResponseEntity", "FormPDFResponseData", "TableResponseCell",
-        "TableSubmissionRow", "FoundationNonprofitCreateRequest", "UpdateAppUserRequest", "DocusignTokenResponse",
-        "DocusignUserFoundationSettings", "NotificationSetting", "NylasAccessToken", "NylasUserFoundationSettings",
-        "SystemNotifications", "UserBrandingSettings", "UserFoundationSettings", "UserPipelineSettings", "Faceting",
-        "Pagination", "SwapIndexesParams", "Task", "TaskDetails", "TaskError", "TypoTolerance", "CandidEntityResponse",
-        "DemographicsCategory", "DemographicsData", "DemographicsDetails", "DemographicsResponse",
-        "DemographicsStaffLevelTotals", "DemographicsSubcategory", "DemographicsSummary", "PremierAccountingRatio",
-        "PremierAccreditation", "PremierAddress", "PremierAnnualReport", "PremierBalanceSheetComposition",
-        "PremierBoardLeadershipPractice", "PremierBoardOfDirector", "PremierBusinessModelIndicators",
-        "PremierCapitalStructureIndicators", "PremierCategory", "PremierCharitycheck", "PremierChartingImpactAnswer",
-        "PremierContractor", "PremierData", "PremierDemographics", "PremierEquityStrategies",
-        "PremierExpenseComposition", "PremierF990Financial", "PremierFeedbackResponse", "PremierFinancialDocument",
-        "PremierFinancialStatement", "PremierFinancialTrendsAnalysis", "PremierFinancials",
-        "PremierFormerlyKnownA", "PremierForms990T", "PremierFullCostComponents", "PremierFundingSource",
-        "PremierHighestPaidEmployee", "PremierKeyDataChecks", "PremierLettersOfDetermination", "PremierLiquidity",
-        "PremierMostRecentYearFinancials", "PremierNaicsCode", "PremierNteeCode", "PremierOfficersDirectorsKeyEmployee",
-        "PremierOperations", "PremierOrgDoesNotCollectFor", "PremierOrganizationDemographics",
-        "PremierOrganizationType", "PremierOtherDocument", "PremierOtherStaff", "PremierPart10BalanceSheet",
-        "PremierPart8Revenue", "PremierPart9Expenses", "PremierPcsCode", "PremierPhoto", "PremierPlatinumEvaluationDocument",
-        "PremierProfileDataChangeDates", "PremierProfileSdgCode", "PremierProfitability", "PremierProgram",
-        "PremierProgramPlatinumMetric", "PremierPrograms", "PremierResponse", "PremierRevenueComposition", "PremierSdg",
-        "PremierSicCode", "PremierStaffLevel", "PremierStaffLevelTotals", "PremierSubcategory", "PremierSummary",
-        "PremierTelephoneNumber", "PremierVideo", "PremierYear", "Exception", "Function0", "Function0Object",
-        "LongRunningTask", "LongRunningTaskObject", "StackTraceElement", "Throwable", "IndexRequest",
-        "AggregateDashboardResponse", "AggregateFoundationProposalSummary", "AggregateGrantAnalysis",
-        "AnvilPDFFieldResponse", "AnvilPDFFieldResponses", "AnvilPaymentDetails", "Currency", "DonorActivity",
-        "DonorActivityAdditionalInfo", "DonorContribution", "DonorContributionComment", "DonorData", "DonorEntity",
-        "DonorRelationship", "DonorTag", "EntityDetails", "FirstFormDetails", "FormBuilder",
-        "FoundationProposalSummary", "GrantAdditionalInfo", "GrantAdditionalInfoEntity", "GrantAmount",
-        "GrantDuration", "GrantEligibility", "GrantFormAdditionalInfo", "GrantFormProposal", "GrantPayment",
-        "GrantPaymentSummary", "GrantProposalSubmission", "GrantRefereeConfig", "GrantRefereeInfo",
-        "GrantRefereeRequestConfig", "GrantRefereeRequestDetail", "GrantSubmissionSearchResponse",
-        "MultiFormResponse", "MultipleFormInfo", "PaymentAdditionalInfo", "PipelineProposalSummary",
-        "ProposalStageResult", "QBPaymentDetails", "SignatureOrder", "SignatureStatusDetails", "TaskAdditionalInfo",
-        "TaskSourceEntity", "TaskTemplate", "TaskTemplateAssignee", "TaskTemplateResponse", "TipTapAttrs",
-        "TipTapTextBlock", "UserDetails", "AggregateDashboardRequest", "DataColumn", "DataRow",
-        "DataSearchResponse", "SubmissionTableResponseRetriever", "AggregateDataSearchRequest", "AwardStatistics",
-        "BudgetCategoryAllocation", "BudgetCategoryYOYData", "CustomGrantTypeAllocation", "CustomGrantTypeYOYData",
-        "DashboardResponse", "FiscalRange", "FiscalYearData", "FoundationTagAllocation", "GrantTypeAllocation",
-        "GrantTypeYOYData", "NonprofitAmount", "TagYOYData", "YOYData", "YearlyData", "AggregateExecDashboardRequest",
-        "AggregateUniversalFoundationSearchResponse", "AggregateUniversalSearchResponse", "CRMSubmissionDetails",
-        "FoundationNonprofitMetadata", "FoundationSearchResponseEntity", "AggregateUniversalSearchQuery",
-        "AggregateContactResponse", "AggregateFoundationCRMResponse", "AggregateContactSearchRequest", "BoardBook",
-        "BoardBookSummarySheet", "BoardGrantProposal", "BoardGeneratorRequest", "BoardProposalValues",
-        "DocumentFieldResponse", "BoardProposalSearchResponse", "BoardProposalSearchQuery", "AnalyticsField",
-        "AnalyticsFieldSubmissionInfo", "DataSearchQuery", "AnalyticsFieldPaymentResponse", "AnalyticsFieldValue",
-        "AnalyticsFieldYOYData", "AnalyticsResponse", "AnalyticsRequest", "DocumentTemplate",
-        "DocumentTemplateAdditionalInfo", "AccountInfo", "DocusignSenderViewResponse", "EnvelopeRecipient",
-        "EnvelopeSendRequest", "DocusignAdditionalInfo", "DocusignEnvelope", "Envelope", "Recipients", "Sender",
-        "Signer", "DocusignLoginURL", "DocusignLoginRequest", "DonorRelationshipType", "DonorEntityType",
-        "EligibilityConfiguration", "EligibilityEmailTemplate", "EligibilityForm", "UniversalForm",
-        "ExternalReviewerDashboard", "TaskSubmission", "AssignExternalReviewerRequest", "FileMeta", "FileCopyPair",
-        "Folder", "AdditionalFilterInfo", "FilterView", "FilterViewCondition", "FilterViewFilter",
-        "FilterViewRename", "FilterViewSettings", "FilterViewSorting", "FilterViewVisibility",
-        "EligibilityFormResponses", "NonprofitGrantPair", "NonprofitGrantSubmission", "GrantEligibilityResponse",
-        "GetFoundationNonprofitUsersResponse", "FoundationPaymentField", "FoundationProposals",
-        "FilteredProposalCell", "FilteredProposalColumn", "FilteredProposalRow", "GetFilteredProposalsResponse",
-        "GetFilteredBulkProposalsResponse", "BulkFilterProposalsRequest", "CreateTaskReviewerRequest",
-        "CRMSearchResponse", "CRMSearchQuery", "FilterOption", "SortOption", "LandingPage", "ContactsResponse",
-        "ContactsRequest", "PaymentSearchResponse", "PaymentSearchQuery", "BankInfoDownloadRequest",
-        "NonprofitMergeRequest", "FoundationNonprofitGrantSummary", "FoundationNonprofitPaymentSummary", "NumbersACH",
-        "FoundationNotes", "AccountBasedExpenseLineDetail", "AttachableRef", "Bill", "ContactInfo", "CustomField",
-        "DepositLineDetail", "DescriptionLineDetail", "DiscountLineDetail", "DiscountOverride", "EmailAddress",
-        "EntityTypeRef", "GenericContactType", "GroupLineDetail", "IntuitAnyType", "ItemAdjustmentLineDetail",
-        "ItemBasedExpenseLineDetail", "ItemReceiptLineDetail", "JournalEntryLineDetail", "Line", "LinkedTxn",
-        "MarkupInfo", "ModificationMetaData", "PaymentLineDetail", "PhysicalAddress", "PurchaseOrderItemLineDetail",
-        "QBSearchResponse", "QBSearchResponseBill", "QuickbooksNonprofitDetails", "RecurringInfo",
-        "RecurringScheduleInfo", "ReferenceType", "ReimburseLineDetail", "SalesItemLineDetail",
-        "SalesOrderItemLineDetail", "SubTotalLineDetail", "TDSLineDetail", "Tag", "TaxLineDetail",
-        "TelephoneNumber", "TxnApprovalInfo", "TxnTaxDetail", "UOMRef", "Vendor", "VendorBankAccountDetail",
-        "WebSiteAddress", "AccountBalance", "AccountBase", "AuthMetadata", "AuthSupportedMethods",
-        "BankAccountDetail", "HealthIncident", "IncidentUpdate", "Institution", "InstitutionStatus",
-        "PaymentInitiationMetadata", "PaymentInitiationStandingOrderMetadata", "ProductStatus",
-        "ProductStatusBreakdown", "BulkNonprofitUserUpdate", "FoundationNonprofitActivity",
-        "GrantSubmissionStatusChange", "NylasFolder", "NylasLabel", "NylasParticipant", "NylasThread",
-        "NylasThreadGroup", "StatusChangeData", "PostmarkMessage", "PostmarkMessageEvent",
-        "PostmarkMessageEventDetails", "PostmarkOutboundSearchResponse", "PostmarkRecipient", "HTMLRequest",
-        "SubmissionRequest", "GrantTemplate", "BulkActionRequest", "BulkCreatePaymentRequest",
-        "BulkCreateReminderRequest", "BulkCreateReportRequest", "SubmissionGranteePair",
-        "GrantSubmissionSearchQuery", "SubmissionComment", "ReviewEntity", "ReviewScores", "UserReviewResponse",
-        "BulkDownloadRequest", "GrantProposalSubmissionDeltaResponse", "GrantSubmissionChange", "SubmissionTasks",
-        "ManualGrantLog", "GrantProposalSubmissionReminder", "NonprofitGrantRefereeStatus",
-        "EntitySuggestionResponse", "ResponseEntitySuggestion", "SubmissionSuggestionResponses",
-        "SuggestionResponseSource", "QuestionAnswerPair", "SuggestedResponsesRequest", "MailChimpAuthUrl",
-        "MailChimpAddress", "MailChimpCampaign", "MailChimpList", "NonprofitCreateRequest", "Notification",
-        "NotificationContent", "NotificationResponse", "NotificationUser", "NotificationQuery", "NylasFile",
-        "NylasMessage", "NylasLoginURL", "NylasFileRequest", "PlaidLinkTokenResponse", "PlaidTokenExchangeResponse",
-        "PlaidTokenExchangeRequest", "QuickbooksLoginURL", "QuickbooksLoginRequest", "Account", "QBSearchResponseAccount",
-        "QBBillSearchRequest", "QBSearchResponseVendor", "QBVendorSearchRequest", "Report", "ReportSubmission",
-        "SubmissionEntity", "SubmissionResponse", "CommunicationEmailTemplate", "CommunicationPortalEmail",
-        "CommunicationPortalEmailNonprofit", "CommunicationPortalEmailRecipients", "CommunicationPortalGetFoundationEmailsResponse",
-        "CommunicationPortalGetFoundationEmailsRequest", "CommunicationPortalGetFoundationEmailResponse",
-        "DocumentItemResponse", "TaskWithSubmission", "SalesforceLoginURL", "SalesforceLoginRequest", "FoundationSearchResponse",
-        "FoundationSearchQuery", "NonprofitSearchEntity", "NonprofitSearchResponse", "NonprofitSearchQuery",
-        "UniversalSearchDocument", "UniversalSearchResponse", "UniversalSearchQuery", "FoundationSearchBarQuery",
-        "TaskSearchResponse", "TaskSearchQuery", "TaskComment", "BulkTaskSubmissionRequest", "UserBearer",
-        "GetNonprofitResult", "GetNonprofitMetadataResult", "GetNonprofitUserResult"
-};
- */
+package org.example;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
@@ -143,11 +13,11 @@ public class Main {
         try {
 
             // Swagger JSON file
-            String swaggerJsonPath = "/Users/uday/Downloads/Temelio/swagger.json";
-            String outputFilePath = "/Users/uday/Downloads/Temelio/output.java";
+//            String swaggerJsonPath = "/Users/uday/Downloads/Temelio/swagger.json";
+//            String outputFilePath = "/Users/uday/Downloads/Temelio/output.java";
 
-//            String swaggerJsonPath = "/Users/uday/Downloads/Temelio/problem2-swagger.json";
-//            String outputFilePath = "/Users/uday/Downloads/Temelio/output2.java";
+            String swaggerJsonPath = "/Users/uday/Downloads/Temelio/problem2-swagger.json";
+            String outputFilePath = "/Users/uday/Downloads/Temelio/output2.java";
 
             // Read Swagger JSON
             String swaggerJson = readSwaggerJson(swaggerJsonPath);
@@ -181,7 +51,7 @@ public class Main {
         StringBuilder javaModels = new StringBuilder();
 
         String[] classNames1 = {
-                // List of class names goes here
+                // List of class names
                 "Nonprofit", "Address", "FiscalSponsor", "NonprofitAdditionalInfo", "NonprofitIRSData", "NonprofitMetadata",
                 "OfacFlags", "OfacSdn", "PlaidData", "DeleteFileInfo", "Foundation", "FoundationUser", "NonprofitUser",
                 "PresignedFile", "UploadedFile", "UserFoundationRole", "UserNonprofitRole", "AppUser", "NylasSyncRequest",
@@ -296,9 +166,10 @@ public class Main {
                 "TaskSearchResponse", "TaskSearchQuery", "TaskComment", "BulkTaskSubmissionRequest", "UserBearer",
                 "GetNonprofitResult", "GetNonprofitMetadataResult", "GetNonprofitUserResult"
         };
+        // for second swagger.json file
         String[] classNames2 = {"Address", "Duration", "GrantSubmission", "Nonprofit"};
 
-        for (String className : classNames1) {
+        for (String className : classNames2) {
             generateClassModel(rootNode, className, javaModels);
         }
 
@@ -477,8 +348,7 @@ public class Main {
             case "integer" -> "int";
             case "number" -> "double";
             case "boolean" -> "boolean";
-            case "array" -> "List"; // Assuming array type will be a list
-            // Handle other types as needed
+            case "array" -> "List";
             default -> "Object";
         };
     }
